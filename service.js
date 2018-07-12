fa76e5b7c6876152636d6a46f334f5c5506b645a
Amio <amio.cn@gmail.com>
2018-07-12 15:23:41 +0800

    Format service.js

--- service.js
+++ service.js
@@ -1,31 +1,30 @@
 const http = require('http')
-const router = require('find-my-way')()
-const setupLiveBadge = require('./libs/setup-live-badge.js')
+const fmw = require('find-my-way')
 const setupRedirectBadge = require('./libs/setup-redirect-badge.js')
+const setupLiveBadge = require('./libs/setup-live-badge.js')
 const serveIndex = require('./libs/serve-index.js')
 const serve404 = require('./libs/serve-404.js')
 const { serveBadge, serveListBadge } = require('./libs/serve-badge.js')
 
+const router = fmw({ defaultRoute: serve404 })
+
+// Setup routes for serving statics
+router.get('/', serveIndex)
 router.get('/badge/:subject/:status', serveBadge)
 router.get('/badge/:subject/:status/:color', serveBadge)
 router.get('/list/:subject/:status', serveListBadge)
 router.get('/list/:subject/:status/:color', serveListBadge)
 
+// Setup routes for serving redirects
 setupLiveBadge(router)
 setupRedirectBadge(router)
 
-router.get('/', serveIndex)
-router.all('/*', serve404)
-
-const rootHandler = (req, res) => {
+http.createServer((req, res) => {
   try {
     router.lookup(req, res)
   } catch (ex) {
-    console.error(ex)
+    console.error('CRITICAL', ex)
     res.statusCode = 500
     res.end()
   }
-}
-
-const server = http.createServer(rootHandler)
-server.listen(3000)
+}).listen(3000)
