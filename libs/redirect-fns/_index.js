25912e0026267f57f88675d1ae93a4e0f65776cd
Amio <amio.cn@gmail.com>
2018-07-13 02:30:11 +0800

    Add redirect badge: travis-org #4

--- libs/redirect-fns/_index.js
+++ libs/redirect-fns/_index.js
@@ -1,5 +1,6 @@
 module.exports = {
-  appveyor: require('./appveyor.js'),
-  circleci: require('./circleci.js'),
-  travis: require('./travis.js')
+  'appveyor': require('./appveyor.js'),
+  'circleci': require('./circleci.js'),
+  'travis': require('./travis.js'),
+  'travis-org': require('./travis-org.js')
 }
