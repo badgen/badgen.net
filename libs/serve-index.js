2e3f47cf0f9155d8ee1cc88a761027b2f169252f
Amio <amio.cn@gmail.com>
2018-07-12 13:01:45 +0800

    Update serve-marked to 0.1.0

--- libs/serve-index.js
+++ libs/serve-index.js
@@ -4,12 +4,9 @@ module.exports = serveMarked('README.md', {
   title: 'Badgen - Fast badge generating service',
   preset: 'merri',
   inlineCSS: `
-    body { color: #333 }
-    a { text-decoration: none; color: #06D }
-    a:hover { text-decoration: underline }
     table { border-spacing: 0 }
-    td { padding: 0 1em 0 0; height: 24px; font: 14px/14px sans-serif }
+    td { padding: 0 1em 0 0 }
     td a { font: 14px/14px monospace; vertical-align: top }
   `,
-  googleAnalyticsID: 'UA-4646421-14'
+  trackingGA: 'UA-4646421-14'
 })
