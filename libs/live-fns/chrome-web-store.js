00111d025ed63c214ae0e3ff69fcd0db3220356f
simov <simeonvelichkov@gmail.com>
2018-07-15 13:20:03 +0300

    Update chrome-web-store badge implementation
    - Bump the chrome-webstore module
    - Update extension's stars rendering

--- libs/live-fns/chrome-web-store.js
+++ libs/live-fns/chrome-web-store.js
@@ -1,8 +1,22 @@
-const chrome = require('chrome-webstore')
+const webstore = require('chrome-webstore')
 const millify = require('millify')
 
+const round = (value, decimals) =>
+  Number(Math.round(value + 'e' + decimals) + 'e-' + decimals)
+
+const stars = (average) => {
+  const base = Math.floor(average)
+  const fraction = average - base
+  return ((full = ''.padEnd(base, '★')) =>
+    // TODO: update when Unicode 11 goes mainstream
+    // between 0.33 and 0.66 should be `half star` symbol
+    fraction >= 0.33 && fraction <= 0.66 ? full.padEnd(base + 1, '★') :
+    fraction > 0.66 ? full.padEnd(base + 1, '★') : full
+  )().padEnd(5, '☆')
+}
+
 module.exports = async function (method, ...args) {
-  const meta = await chrome.extension({id: args[0]})
+  const meta = await webstore.detail({id: args[0]})
   switch (method) {
     case 'v':
       return {
@@ -13,32 +27,31 @@ module.exports = async function (method, ...args) {
     case 'users':
       return {
         subject: 'users',
-        status: millify(parseInt(meta.interactionCount.replace(',', ''))),
+        status: millify(parseInt(meta.users.replace(',', ''))),
         color: 'green'
       }
     case 'price':
       return {
         subject: 'price',
-        status: `$${meta.price}`,
+        status: meta.price,
         color: 'green'
       }
     case 'rating':
       return {
         subject: 'rating',
-        status: `${meta.ratingValue}/5`,
+        status: `${round(meta.rating.average, 2)}/5`,
         color: 'green'
       }
     case 'stars':
       return {
         subject: 'stars',
-        status: Array(Math.ceil(meta.ratingValue)).fill('★').concat(
-          Array(5 - Math.ceil(meta.ratingValue)).fill('☆')).join(''),
+        status: stars(meta.rating.average),
         color: 'green'
       }
     case 'rating-count':
       return {
         subject: 'rating count',
-        status: `${meta.ratingCount} total`,
+        status: `${meta.rating.count} total`,
         color: 'green'
       }
     default:
