b82b718179489d4d4c19ba131a293de67275b068
Amio <amio.cn@gmail.com>
2018-07-17 21:51:55 +0800

    Update david-dm with dev/peer/optional support #11

--- libs/live-fns/david.js
+++ libs/live-fns/david.js
@@ -5,17 +5,41 @@ const statusInfo = {
   'outofdate': ['out of date', 'orange'],
   'notsouptodate': ['up to date', 'yellow'],
   'uptodate': ['up to date', 'green'],
-  'none': ['pending', 'grey']
+  'none': ['none', 'blue']
 }
 
 module.exports = async function (depType, user, repo) {
-  const endpoint = `https://david-dm.org/${user}/${repo}/info.json`
+  const prefix = {
+    'dep': '',
+    'dev': 'dev-',
+    'peer': 'peer-',
+    'optional': 'optional-'
+  }[depType]
+  const endpoint = `https://david-dm.org/${user}/${repo}/${prefix}info.json`
   const { status } = await axios.get(endpoint).then(res => res.data)
 
   switch (depType) {
     case 'dep':
       return {
         subject: 'dependencies',
+        status: statusInfo[status][0],
+        color: statusInfo[status][1]
+      }
+    case 'dev':
+      return {
+        subject: 'devDependencies',
+        status: statusInfo[status][0],
+        color: statusInfo[status][1]
+      }
+    case 'peer':
+      return {
+        subject: 'peerDependencies',
+        status: statusInfo[status][0],
+        color: statusInfo[status][1]
+      }
+    case 'optional':
+      return {
+        subject: 'optionalDependencies',
         status: statusInfo[status][0],
         color: statusInfo[status][1]
       }
