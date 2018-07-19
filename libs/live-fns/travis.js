9641b9d534abeaad54b50022f27eb9de95ac6e44
Charlike Mike Reagent <olsten.larck@gmail.com>
2018-07-19 15:05:29 +0300

    fix: revert changes that are not for here
    Signed-off-by: Charlike Mike Reagent <olsten.larck@gmail.com>

--- libs/live-fns/travis.js
+++ libs/live-fns/travis.js
@@ -3,12 +3,12 @@ const axios = require('../axios.js')
 module.exports = async function (user, repo, branch = 'master') {
   const com = `https://api.travis-ci.com/${user}/${repo}.svg?branch=${branch}`
   const org = `https://api.travis-ci.org/${user}/${repo}.svg?branch=${branch}`
-  const res = await Promise.all([
+  const results = await Promise.all([
     axios.get(com).then(res => res.data).catch(e => e),
     axios.get(org).then(res => res.data).catch(e => e)
   ])
 
-  if (res[0].match(/passed|passing/) || res[1].match(/passed|passing/)) {
+  if (results[0].match('passing') || results[1].match('passing')) {
     return {
       subject: 'build',
       status: 'passing',
@@ -16,7 +16,7 @@ module.exports = async function (user, repo, branch = 'master') {
     }
   }
 
-  if (res[0].match(/failed|failing/) || res[1].match(/failed|failing/)) {
+  if (results[0].match('failed') || results[1].match('failed')) {
     return {
       subject: 'build',
       status: 'failed',
