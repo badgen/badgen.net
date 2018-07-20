3f55c30137cd1911a176e97acf6db400e52e3027
Fox George Penguin <5038030+olstenlarck@users.noreply.github.com>
2018-07-20 17:29:45 +0300

    feat: start github live badges (#15)
    * feat: start github live badges
    
    Signed-off-by: Charlike Mike Reagent <olsten.larck@gmail.com>
    
    * fix: sort keys
    
    Signed-off-by: Charlike Mike Reagent <olsten.larck@gmail.com>
    
    * fix: remove sort-keys rule
    
    Signed-off-by: Charlike Mike Reagent <olsten.larck@gmail.com>
    
    * fix: address comments - orange color on prerelease
    
    Signed-off-by: Charlike Mike Reagent <olsten.larck@gmail.com>

--- libs/live-fns/_index.js
+++ libs/live-fns/_index.js
@@ -1,10 +1,11 @@
 module.exports = {
-  'codecov': require('./codecov.js'),
   'appveyor': require('./appveyor.js'),
   'chrome-web-store': require('./chrome-web-store.js'),
   'circleci': require('./circleci.js'),
+  'codecov': require('./codecov.js'),
   'crates': require('./crates.js'),
   'david': require('./david.js'),
+  'github': require('./github.js'),
   'homebrew': require('./homebrew.js'),
   'npm': require('./npm.js'),
   'travis': require('./travis.js')
