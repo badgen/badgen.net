a8f1b70a799fbb1d9f4367ac79aef91d45a93c5f
Amio / <amio.cn@gmail.com>
2018-07-28 16:16:18 +0800

    New index page (#43)
    * feat: new index page
    
    * docs: complete example list for new index #43
    
    * docs: add anchor to example titles #43
    
    * docs: refine anchor style for firefox

--- libs/serve-index.js
+++ libs/serve-index.js
@@ -4,29 +4,30 @@ module.exports = serveMarked('libs/index.md', {
   title: 'Badgen - Fast badge generating service',
   preset: 'merri',
   inlineCSS: `
-    body { max-width: 890px; padding-bottom: 1em }
-    table { border-spacing: 0 }
-    td { padding: 0 1em 0 0; font-size: 14px; white-space: nowrap; height: 26px }
-    td img { height: 20px; position: relative; top: 2px }
-    td a { font: 12px/14px monospace }
-    pre, code { background-color: #f4f6f9; font-weight: 400 }
-    li code { padding: 0.2em 0.4em; display: pre }
-    li { padding: 0.4em 0 }
+    body { max-width: 950px; padding: 0 1.6em 1em }
     h1 + p { letter-spacing: 0.1px }
     h1 + p a { display: inline-block; margin-top: 1em; padding: 4px; height: 20px }
     h1 + p img { height: 20px }
+
+    table { border-spacing: 0; vertical-align: top }
+    td { padding-right: 0.6em; height: 28px; font-size: 14px; white-space: nowrap }
+    td img { position: relative; top: 2px }
+    td a { font: 14px/20px monospace }
+    pre, code { background-color: #f2f5f9; font-weight: 400 }
+    pre > code { padding: 0 }
+    table code { padding: 0.3em 0.5em; display: pre }
+
+    dl { margin-top: 0 }
+    dt { margin-bottom: 1em; padding-top: 1em; border-bottom: 1px solid #DDD; line-height: 2em }
+    dt a { color: #333; position: relative }
+    dt a:hover { text-decoration: none }
+    dt a:hover:before { content: '➻'; display: inline-block; width: 0px; position: relative }
+    dt a:hover:before { left: -1.2em; font: 20px/20px Arial; vertical-align: middle; color: #CCC }
+    dd { font: 14px/20px monospace; vertical-align: top; height: 28px; white-space: nowrap; margin: 0 }
+    dd img { vertical-align: top }
+    dd b { display: inline-block; min-width: 14em; text-align: right; font-weight: 300 }
+    dd i { display: inline-block; min-width: 13em }
   `,
   beforeHeadEnd: `<link rel="icon" type="image/svg+xml" href="/favicon.svg">`,
-  beforeBodyEnd: `
-    <script>
-      if (window.location.hostname === 'flat.badgen.net') {
-        const code = document.querySelector('pre code')
-        code.innerText = code.innerText.replace(
-          'badgen.net',
-          'flat.badgen.net'
-        ).replace(/\\n/g, '\\n     ')
-      }
-    </script>
-  `,
   trackingGA: 'UA-4646421-14'
 })
