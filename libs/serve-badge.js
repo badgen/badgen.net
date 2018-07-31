8858f22c3b7cc38d1404bbcebc9f2e6c039e89d0
Amio <amio.cn@gmail.com>
2018-07-31 21:21:04 +0800

    feat: generate badge before setHeader

--- libs/serve-badge.js
+++ libs/serve-badge.js
@@ -11,16 +11,16 @@ module.exports = function serveBadge (req, res, options = {}) {
   const { subject, status, color } = req.params
   const { style, label, emoji, list, icon } = req.query
 
-  const badgenParams = {
+  const badge = badgen({
     subject: typeof label !== 'undefined' ? label : subject,
     status: String(list ? status.replace(/,/g, ' | ') : status),
     color: color,
     style: style || hostStyle,
     emoji: Boolean(emoji),
     icon: builtin[icon]
-  }
+  })
 
   res.setHeader('Content-Type', 'image/svg+xml;charset=utf-8')
   res.setHeader('Cache-Control', `${CACHE_CONTROL}, s-maxage=${maxAge}`)
-  send(res, code, badgen(badgenParams))
+  send(res, code, badge)
 }
