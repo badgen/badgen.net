1a8e9db9a9d1f363990acfa4e2d9e64e4680a779
Clark Du <clark.du@betbright.com>
2018-08-03 15:51:39 +0100

    refactor: return correct default unknownBadgeData

--- libs/live-fns/nuget.js
+++ libs/live-fns/nuget.js
@@ -4,15 +4,15 @@ const semColor = require('../utils/sem-color.js')
 module.exports = async function (method, project, channel) {
   const endpoint = `https://api.nuget.org/v3-flatcontainer/${project}/index.json`
   const { versions } = await axios.get(endpoint).then(res => res.data)
+  const unknownBadgeData = {
+    subject: 'nuget',
+    status: 'unknown',
+    color: 'grey'
+  }
 
   switch (method) {
     case 'v':
-      var version = ''
-      var unknownBadgeData = {
-        subject: 'nuget',
-        status: 'unknown',
-        color: 'grey'
-      }
+      let version = ''
 
       switch (channel) {
         case 'latest':
