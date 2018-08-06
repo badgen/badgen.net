15d54ed8c6ec2e75ec74477c0d2e91a367b039a8
Jacob Müller <jacob.mueller.elz@gmail.com>
2018-08-06 17:43:55 +0200

    Use `semver` to sort versions

--- libs/utils/compare-versions.js
+++ libs/utils/compare-versions.js
@@ -1,44 +1,39 @@
-const indexOrEnd = (str, q) => str.indexOf(q) === -1 ? str.length : str.indexOf(q)
-const parseNumber = v => isNaN(Number(v)) ? v : Number(v)
+const semver = require('semver')
 
-const split = (v) => {
-  var c = v.replace(/^v/, '').replace(/\+.*$/, '')
-  var patchIndex = indexOrEnd(c, '-')
-  var arr = c.substring(0, patchIndex).split('.')
-  arr.push(c.substring(patchIndex + 1))
-  return arr
-}
-
-// See: https://github.com/omichelsen/compare-versions
 module.exports = (v1, v2) => {
-  var s1 = split(v1)
-  var s2 = split(v2)
+  const validV1 = semver.valid(v1)
+  const validV2 = semver.valid(v2)
 
-  for (var i = 0; i < Math.max(s1.length - 1, s2.length - 1); i++) {
-    var n1 = parseInt(s1[i] || 0, 10)
-    var n2 = parseInt(s2[i] || 0, 10)
+  const isV1Valid = !!validV1
+  const isV2Valid = !!validV2
 
-    if (n1 > n2) return 1
-    if (n2 > n1) return -1
-  }
+  if (!isV1Valid && !isV2Valid) {
+    const numberVersionRegex = /^\d/
+    const isV1NumberVersion = numberVersionRegex.test(v1)
+    const isV2NumberVersion = numberVersionRegex.test(v2)
 
-  var sp1 = s1[s1.length - 1]
-  var sp2 = s2[s2.length - 1]
-
-  if (sp1 && sp2) {
-    var p1 = sp1.split('.').map(parseNumber)
-    var p2 = sp2.split('.').map(parseNumber)
-
-    for (i = 0; i < Math.max(p1.length, p2.length); i++) {
-      if (p1[i] === undefined || (typeof p2[i] === 'string' && typeof p1[i] === 'number')) return -1
-      if (p2[i] === undefined || (typeof p1[i] === 'string' && typeof p2[i] === 'number')) return 1
-
-      if (p1[i] > p2[i]) return 1
-      if (p2[i] > p1[i]) return -1
+    if (isV1NumberVersion && isV2NumberVersion) {
+      return v1.localeCompare(v2)
     }
-  } else if (sp1 || sp2) {
-    return sp1 ? -1 : 1
+
+    if (isV1NumberVersion && !isV2NumberVersion) {
+      return 1
+    }
+    if (!isV1NumberVersion && isV2NumberVersion) {
+      return -1
+    }
   }
 
-  return 0
+  if (isV1Valid && isV2Valid) {
+    return semver.compare(validV1, validV2)
+  }
+
+  if (isV1Valid && !isV2Valid) {
+    return 1
+  }
+  if (!isV1Valid && isV2Valid) {
+    return -1
+  }
+
+  return v1.localeCompare(v2)
 }
