d7a7cc93dbee4c539ab0aabfb851b06723af4624
Jacob Müller <jacob.mueller.elz@gmail.com>
2018-08-06 18:07:53 +0200

    Make use of `semver.coerce()`

--- libs/utils/compare-versions.js
+++ libs/utils/compare-versions.js
@@ -1,28 +1,11 @@
 const semver = require('semver')
 
 module.exports = (v1, v2) => {
-  const validV1 = semver.valid(v1)
-  const validV2 = semver.valid(v2)
+  const validV1 = semver.valid(semver.coerce(v1))
+  const validV2 = semver.valid(semver.coerce(v2))
 
   const isV1Valid = !!validV1
   const isV2Valid = !!validV2
-
-  if (!isV1Valid && !isV2Valid) {
-    const numberVersionRegex = /^\d/
-    const isV1NumberVersion = numberVersionRegex.test(v1)
-    const isV2NumberVersion = numberVersionRegex.test(v2)
-
-    if (isV1NumberVersion && isV2NumberVersion) {
-      return v1.localeCompare(v2)
-    }
-
-    if (isV1NumberVersion && !isV2NumberVersion) {
-      return 1
-    }
-    if (!isV1NumberVersion && isV2NumberVersion) {
-      return -1
-    }
-  }
 
   if (isV1Valid && isV2Valid) {
     return semver.compare(validV1, validV2)
