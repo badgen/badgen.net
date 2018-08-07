6cdee9b71a65a3aed0d95aec3f74c19ef4cca37a
Amio <amio.cn@gmail.com>
2018-08-07 20:33:24 +0800

    feat: update runkit schema #24

--- libs/live-fns/runkit.js
+++ libs/live-fns/runkit.js
@@ -1,7 +1,15 @@
 const axios = require('../axios.js')
 
-module.exports = async function (account, notebook, ...args) {
-  const endpoint = `https://runkit.io/${account}/${notebook}/branches/master?args=${args.join('/')}`
+/**
+ * EXAMPLES
+ *
+ *     /runkit/satisfaction-flq08o9mm3ka/102909/employee
+ *     /runkit/satisfaction-flq08o9mm3ka/102909/people
+ *     /runkit/satisfaction-flq08o9mm3ka/102909/topic
+ */
+
+module.exports = async function (endpointId, ...args) {
+  const endpoint = `https://${endpointId}.runkit.sh/${args.join('/')}`
   const { subject, status, color } = await axios(endpoint).then(res => res.data)
 
   if (subject && status) {
