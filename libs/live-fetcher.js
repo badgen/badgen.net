eaa50f3c6ccf24214bec20c534ece28e0f902fe2
Jacob Müller <jacob.mueller.elz@gmail.com>
2018-08-10 15:01:11 +0200

    Revert another multiline change

--- libs/live-fetcher.js
+++ libs/live-fetcher.js
@@ -5,24 +5,22 @@ module.exports = async (scope, fn, paramsPath) => {
   if (waitings[fetchKey]) return waitings[fetchKey]
 
   console.time(fetchKey)
-  waitings[fetchKey] = fn(...paramsPath.split('/'))
-    .catch(e => {
-      let status = 'unknown'
+  waitings[fetchKey] = fn(...paramsPath.split('/')).catch(e => {
+    let status = 'unknown'
 
-      if (e.response && e.response.status === 404) {
-        status = 'not found'
-      } else if (e.code === 'ECONNABORTED') {
-        status = 'timeout'
-      }
+    if (e.response && e.response.status === 404) {
+      status = 'not found'
+    } else if (e.code === 'ECONNABORTED') {
+      status = 'timeout'
+    }
 
-      console.error(fetchKey, `LIVEFN_ERR<${status}>`, e.message)
-      return { status, failed: true }
-    })
-    .then(result => {
-      console.timeEnd(fetchKey)
-      waitings[fetchKey] = undefined
-      return result || { failed: true }
-    })
+    console.error(fetchKey, `LIVEFN_ERR<${status}>`, e.message)
+    return { status, failed: true }
+  }).then(result => {
+    console.timeEnd(fetchKey)
+    waitings[fetchKey] = undefined
+    return result || { failed: true }
+  })
 
   return waitings[fetchKey]
 }
