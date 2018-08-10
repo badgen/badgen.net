51227745b60d5e3a32df7d97ebc609255c2bbf58
Amio <amio.cn@gmail.com>
2018-08-10 22:55:36 +0800

    fix: (live-fns) coveralls percentage bug #96

--- libs/live-fns/coveralls.js
+++ libs/live-fns/coveralls.js
@@ -2,22 +2,39 @@ const axios = require('../axios.js')
 const covColor = require('../utils/cov-color.js')
 
 module.exports = async (topic, platform, user, repo, branch) => {
-  const query = branch ? `?branch=${branch}` : ''
-  const endpoint = `https://coveralls.io/${platform}/${user}/${repo}.json${query}`
-
-  /* eslint-disable camelcase */
-  const { covered_percent } = await axios.get(endpoint).then(res => res.data)
-
   switch (topic) {
     case 'c':
-      return {
-        subject: 'coverage',
-        status: Number(covered_percent.toFixed(1)) + '%',
-        color: covColor(covered_percent)
-      }
+      return coverage(platform, user, repo, branch)
     default:
       return {
         status: 'unknown topic'
       }
+  }
+}
+
+// Detect coveralls.io's badge redirection instead of using it's api
+// See https://github.com/amio/badgen-service/issues/96
+const coverage = async (platform, user, repo, branch) => {
+  const query = branch ? `?branch=${branch}` : ''
+  const badgeURL = await axios({
+    url: `https://coveralls.io/repos/${platform}/${user}/${repo}/badge.svg${query}`,
+    method: 'head',
+    maxRedirects: 0,
+    // Expecting 302 redirection to "coveralls_xxx.svg"
+    validateStatus: status => status >= 200 && status < 400
+  }).then(res => res.headers.location)
+
+  try {
+    const percentage = badgeURL.match(/_(\d+)\.svg/)[1]
+    return {
+      subject: 'coverage',
+      status: percentage + '%',
+      color: covColor(Number(percentage))
+    }
+  } catch (e) {
+    return {
+      subject: 'coverage',
+      status: 'invalid'
+    }
   }
 }
