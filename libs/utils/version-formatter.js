56e86dcf440c5986c6a4a7004dd883756a48b2e6
Jacob Müller <jacob.mueller.elz@gmail.com>
2018-08-10 14:40:22 +0200

    Handle more edge cases in version formatter

--- libs/utils/version-formatter.js
+++ libs/utils/version-formatter.js
@@ -6,11 +6,15 @@
  *   'v1.2.3' => 'v1.2.3'
  *   'dev-master' => 'dev-master',
  *   '' => 'unknown'
+ *   undefined => 'unknown'
+ *   0 => 'v0'
  */
 module.exports = (version) => {
-  if (!version) {
+  if (!version && version !== 0) {
     return 'unknown'
   }
+
+  version = String(version)
 
   const firstChar = version.charAt(0)
   if (firstChar === 'v' || isNaN(parseInt(firstChar, 10))) {
