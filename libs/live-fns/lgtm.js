70cb5877a58210097fb0dd2897e00fca2ce5ebe7
Sam Lanning <sam@samlanning.com>
2018-08-29 11:57:48 -0700

    live-fns: (lgtm) introduce lgtm service

---
+++ libs/live-fns/lgtm.js
@@ -0,0 +1,49 @@
+const millify = require('millify')
+
+// Use axios directly as the default Accept header breaks lgtm API
+const axios = require('axios')
+
+module.exports = async (topic, ...args) => {
+  const lang = topic === 'grade' ? args.shift() : undefined
+  const projectId = args.join('/')
+  const endpoint = `https://lgtm.com/api/v0.1/project/${projectId}/details`
+  const data = await axios.create({
+    timeout: 3200
+  }).get(endpoint).then(res => res.data)
+
+  console.log('got data', data)
+
+  switch (topic) {
+    case 'alerts':
+      return {
+        subject: 'lgtm',
+        status: `${millify(data.alerts)} alert${(data.alerts === 1 ? '' : 's')}`,
+        color: data.alerts === 0 ? 'green' : 'yellow'
+      }
+    case 'grade':
+      for (const languageData of data.languages) {
+        if (languageData.lang === lang && 'grade' in languageData) {
+          const langLabel = langLabelOverrides[lang] || lang
+          return {
+            subject: `code quality: ${langLabel}`,
+            status: languageData.grade,
+            color: gradeColors[languageData.grade] || 'red'
+          }
+        }
+      }
+  }
+}
+
+const langLabelOverrides = {
+  cpp: 'c/c++',
+  csharp: 'c#',
+  javascript: 'js/ts'
+}
+
+const gradeColors = {
+  'A+': 'green',
+  'A': '9C0',
+  'B': 'A4A61D',
+  'C': 'yellow',
+  'D': 'orange'
+}
