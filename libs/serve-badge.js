550a13639d53da1661d4452ed84d8b576a7b8c31
Amio <amio.cn@gmail.com>
2018-08-31 10:17:41 +0800

    fix: update badgen for fixing sanitization #127

--- libs/serve-badge.js
+++ libs/serve-badge.js
@@ -12,7 +12,7 @@ module.exports = (req, res, options = {}) => {
   const { style, label, list, icon } = req.query
 
   const badge = badgen({
-    subject: (typeof label !== 'undefined' ? label : subject).replace('<', '&lt;'),
+    subject: typeof label !== 'undefined' ? label : subject,
     status: String(list ? status.replace(/,/g, ' | ') : status),
     color: color,
     style: style || hostStyle,
