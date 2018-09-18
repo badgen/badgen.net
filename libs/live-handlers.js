1dc50713d266ab2a4c155d3e552e7ed61c717cb3
Amio <amio.cn@gmail.com>
2018-09-18 17:44:18 +0800

    refactor: cleanup api handler

--- libs/live-handlers.js
+++ libs/live-handlers.js
@@ -1,19 +1,7 @@
 const { get } = require('micro-fork')
-const got = require('./got.js')
 const liveFns = require('./live-fns/_index.js')
 const serveBadge = require('./serve-badge.js')
 const liveFetcher = require('./live-fetcher.js')
-
-const { API_HOST } = process.env
-const apiFetcher = async url => {
-  return got(API_HOST + url).then(
-    res => res.body,
-    err => {
-      console.error('API_ERR', url, err.message)
-      return { failed: true, ...(err.response && err.response.body) }
-    }
-  )
-}
 
 module.exports = Object.entries(liveFns).map(([name, fn]) => {
   return get(`/${name}/*`, async (req, res) => {
@@ -22,15 +10,13 @@ module.exports = Object.entries(liveFns).map(([name, fn]) => {
       status = 'unknown',
       color = 'grey',
       failed = false
-    } = await (
-      API_HOST ? apiFetcher(req.url) : liveFetcher(name, fn, req.params['*'])
-    )
+    } = await liveFetcher(name, fn, req.params['*'])
 
     const style = req.headers.host === 'flat.badgen.net' ? 'flat' : undefined
     req.params = { subject, status, color, style }
     serveBadge(req, res, {
       code: 200, // always 200 for badges
-      sMaxAge: failed ? '0' : (Math.random() * 60 + 60).toFixed()
+      sMaxAge: failed ? '0' : '360'
     })
   })
 })
