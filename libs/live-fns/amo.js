5b838b21e72f2030fcdd1a14772be478e83aab89
Bertrand Marron <bertrand.marron@gmail.com>
2019-03-01 17:16:06 +0100

    live-fns: update amo (#255)
    * Update amo
    
    * Add slash to prevent 301

--- libs/live-fns/amo.js
+++ libs/live-fns/amo.js
@@ -1,4 +1,3 @@
-const xml2js = require('xml2js')
 const got = require('../got.js')
 const millify = require('millify')
 const stars = require('../utils/stars.js')
@@ -6,45 +5,38 @@ const semColor = require('../utils/sem-color.js')
 const v = require('../utils/version-formatter.js')
 
 module.exports = async (topic, name) => {
-  const endpoint = `https://services.addons.mozilla.org/en-US/firefox/api/1.5/addon/${name}`
-  const xml = await got(endpoint, { json: false }).then(res => res.body)
-
-  const { addon } = await new Promise((resolve, reject) => {
-    xml2js.parseString(xml, {
-      trim: true,
-      explicitArray: false
-    }, (err, res) => err ? reject(err) : resolve(res))
-  })
+  const endpoint = `https://addons.mozilla.org/api/v3/addons/addon/${name}/`
+  const addon = await got(endpoint).then(res => res.body)
 
   switch (topic) {
     case 'v':
       return {
         subject: 'mozilla add-on',
-        status: v(addon.version),
-        color: semColor(addon.version)
+        status: v(addon.current_version.version),
+        color: semColor(addon.current_version.version)
       }
     case 'users':
       return {
         subject: 'users',
-        status: millify(parseInt(addon.daily_users)),
+        status: millify(parseInt(addon.average_daily_users)),
         color: 'green'
       }
     case 'rating':
       return {
         subject: 'rating',
-        status: `${Number(addon.rating).toFixed(2)}/5`,
+        status: `${Number(addon.ratings.average).toFixed(2)}/5`,
         color: 'green'
       }
     case 'stars':
       return {
         subject: 'stars',
-        status: stars(addon.rating),
+        status: stars(addon.ratings.average),
         color: 'green'
       }
     case 'reviews':
       return {
         subject: 'reviews',
-        status: addon.reviews.$.num,
+        status: addon.ratings.count,
         color: 'green'
       }
     default:
