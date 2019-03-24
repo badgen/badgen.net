7413e84d4e27bf496f9fe353c6cca97302af906c
Andrew Leedham <AndrewLeedham@outlook.com>
2019-03-24 15:01:35 +0000

    live-fns: simplify circleci badge - success/failed (#269)

--- libs/live-fns/circleci.js
+++ libs/live-fns/circleci.js
@@ -4,7 +4,7 @@ const got = require('../got.js')
 // https://circleci.com/docs/api/v1-reference/
 module.exports = async (vcsType, user, project, branch) => {
   branch = branch ? `/tree/${qs.escape(branch)}` : ''
-  const endpoint = `https://circleci.com/api/v1.1/project/${vcsType}/${user}/${project}${branch}`
+  const endpoint = `https://circleci.com/api/v1.1/project/${vcsType}/${user}/${project}${branch}?filter=completed&limit=1`
   const [latest] = await got(endpoint).then(res => res.body)
 
   return {
@@ -16,26 +16,9 @@ module.exports = async (vcsType, user, project, branch) => {
 
 const getStatusColor = status => {
   switch (status) {
-    case 'infrastructure_fail':
-    case 'timedout':
     case 'failed':
-    case 'no_tests':
       return 'red'
 
-    case 'canceled':
-    case 'not_run':
-    case 'not_running':
-      return 'grey'
-
-    case 'queued':
-    case 'scheduled':
-      return 'yellow'
-
-    case 'retried':
-    case 'running':
-      return 'orange'
-
-    case 'fixed':
     case 'success':
       return 'green'
 
