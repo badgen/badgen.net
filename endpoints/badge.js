2ccc8ac854b6b82c7e0160c52b80058edc00f294
amio <amio.cn@gmail.com>
2019-05-13 09:33:08 +0800

    Add static badge examples

--- endpoints/badge.js
+++ endpoints/badge.js
@@ -1,9 +1,14 @@
-const url = require('url')
-const qs = require('querystring')
 const badgenServe = require('../libs/badgen-serve.js')
 
-const help = ``
-const examples = []
+const examples = [
+  '/badge/Swift/4.2/orange',
+  '/badge/license/MIT/blue',
+  '/badge/chat/on%20gitter/cyan',
+  '/badge/stars/★★★★☆',
+  '/badge/become/a%20patron/F96854',
+  '/badge/code%20style/standard/f2a',
+  '/badge/platform/ios,macos,tvos?list=1'
+]
 
 const schemes = [
   '/badge/:label/:status',
