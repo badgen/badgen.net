b3722018b9b5ff464722ef4cc0349e4fcd32e926
amio <amio.cn@gmail.com>
2019-05-16 21:59:36 +0800

    Add types for `examples`

--- libs/serve-help.ts
+++ libs/serve-help.ts
@@ -1,4 +1,14 @@
-export default function serveHelp (req, res, id, { help, examples, handlers }) {
+import { BadgenServeHandlers } from './badgen-serve'
+
+type BadgenExample = [string, string]
+type BadgenHelpParams = {
+  help?: any
+  examples?: BadgenExample[]
+  handlers: BadgenServeHandlers
+}
+
+export default function serveHelp (req, res, id, params: BadgenHelpParams) {
+  const { help, examples = [], handlers } = params
   const Docs = help ? help : `# ${id}`
   const Schemes = `## Schemes\n\n${Object.keys(handlers).join('\n')}`
   const Examples = `## Examples\n\n${examples.map(ex => ex[0]).join('\n')}`
