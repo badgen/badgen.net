50e1b9a1c4e0f2e9a83c96f16824b3ab2a55d91b
Amio <amio.cn@gmail.com>
2019-05-26 21:12:25 +0800

    Use tap for unit testing

--- test/match-route.unit.ts
+++ test/match-route.unit.ts
@@ -1,31 +1,68 @@
+import tap from 'tap'
 import matchRoute from '../libs/match-route'
 
-const matcher = route => path => {
-  const result = matchRoute(route, path)
-  console.log(route, '\n<=', path, '\n=>', result, '\n')
-  return result
+const runTest = (t, route, suite) => {
+  // @ts-ignore
+  Object.entries(suite).forEach(([url, result]) => {
+    t.strictSame(
+      matchRoute(route, url),
+      result,
+      `test match ${url}`
+    )
+  })
+  t.end()
 }
 
-const route = '/github/:topic<stars|forks>/:owner/:repo/:chanel?/:more?'
-const parsed = [
-  '/github/stars/amio/',
-  '/github/eiyou/amio/badgen',
-  '/github/stars/amio/badgen',
-  '/github/stars/amio/badgen',
-  '/github/stars/amio/badgen/master',
-  '/github/stars/amio/badgen/master/',
-  '/github/stars/amio/badgen/master/more',
-].map(matcher(route))
+const testRouteSimple = '/user/:id<\\d+>'
+tap.test(testRouteSimple, t => {
+  const suite = {
+    '/user/eiyo': null,
+    '/user/123': { id: '123' },
+    '/user/123/': { id: '123' }
+  }
+  runTest(t, testRouteSimple, suite)
+})
 
-const route2 = '/user/:id<\\d+>'
-const parsed2 = [
-  '/user/123',
-  '/user/eiyo'
-].map(matcher(route2))
+const testRouteMultiRegex = '/user/:id<\\d+>/:status?<active|inactive>'
+tap.test(testRouteMultiRegex, t => {
+  const suite = {
+    '/user/eiyo': null,
+    '/user/123': { id: '123' },
+    '/user/123/active': { id: '123', status: 'active'},
+    '/user/123/eiyo': null
+  }
+  runTest(t, testRouteMultiRegex, suite)
+})
 
-const route3 = '/usr/:id<\\d+>/:status?<active|inactive>'
-const parsed3 = [
-  '/usr/123',
-  '/usr/123/active',
-  '/usr/123/eiyo'
-].map(matcher(route3))
+const testRouteGithub = '/github/:topic<stars|forks>/:owner/:repo/:chanel?/:more?'
+tap.test(testRouteGithub, t => {
+  const suite = {
+    '/github/stars/amio/': null,
+    '/github/eiyou/amio/badgen': null,
+    '/github/stars/amio/badgen': {
+      topic: 'stars',
+      owner: 'amio',
+      repo: 'badgen'
+    },
+    '/github/stars/amio/badgen/master': {
+      topic: 'stars',
+      owner: 'amio',
+      repo: 'badgen',
+      chanel: 'master'
+    },
+    '/github/stars/amio/badgen/master/': {
+      topic: 'stars',
+      owner: 'amio',
+      repo: 'badgen',
+      chanel: 'master'
+    },
+    '/github/stars/amio/badgen/master/more': {
+      topic: 'stars',
+      owner: 'amio',
+      repo: 'badgen',
+      chanel: 'master',
+      more: 'more'
+    },
+  }
+  runTest(t, testRouteGithub, suite)
+})
