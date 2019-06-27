5443360f6c1bf48622ec30ec18337514f888bca3
Amio <amio.cn@gmail.com>
2019-06-27 09:06:26 +0800

    Update sentry dsn

--- libs/sentry.ts
+++ libs/sentry.ts
@@ -1,9 +1,9 @@
 import * as Sentry from '@sentry/node'
 import { Dedupe } from '@sentry/integrations'
 
-if (process.env.SENTRY_URI) {
+if (process.env.SENTRY_DSN) {
   Sentry.init({
-    dsn: process.env.SENTRY_URI,
+    dsn: process.env.SENTRY_DSN,
     integrations: [
       new Dedupe()
     ]
