c82e5bd169deb6bbe31337f5f46819405e576770
Charlike Mike Reagent <opensource@tunnckocore.com>
2020-02-01 07:47:43 +0200

    chore: typo, copypasta
    Signed-off-by: Charlike Mike Reagent <opensource@tunnckocore.com>

--- api/github.ts
+++ api/github.ts
@@ -127,7 +127,7 @@ async function checks ({ owner, repo, ref = 'master', context}: PathArgs) {
 
       return checkName || appName
     })
-    : resp!.state
+    : resp.check_runs
 
   if (Array.isArray(state)) {
     state = combined(state, 'conclusion')
