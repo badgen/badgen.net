2327defb27b889f77197a497476fcd40db54b604
Dario Vladovic <d.vladimyr@gmail.com>
2020-04-25 06:11:08 +0200

    Add homebrew download badges

--- api/homebrew.ts
+++ api/homebrew.ts
@@ -1,5 +1,5 @@
 import got from '../libs/got'
-import { version, versionColor } from '../libs/utils'
+import { millify, version, versionColor } from '../libs/utils'
 import { createBadgenHandler, PathArgs } from '../libs/create-badgen-handler'
 
 export default createBadgenHandler({
@@ -7,26 +7,46 @@ export default createBadgenHandler({
   examples: {
     '/homebrew/v/fish': 'version',
     '/homebrew/v/cake': 'version',
+    '/homebrew/dm/fish': 'monthly downloads',
+    '/homebrew/dy/fish': 'yearly downloads',
     '/homebrew/cask/v/atom': 'version',
     '/homebrew/cask/v/whichspace': 'version'
+    // NOTE: cask analytics are broken
+    // '/homebrew/cask/dm/atom': 'monthly downloads',
+    // '/homebrew/cask/dy/atom': 'yearly downloads'
   },
   handlers: {
-    '/homebrew/v/:pkg': handler,
-    '/homebrew/:type<formula|cask>/v/:pkg': handler
+    '/homebrew/:topic<v|dm|dy>/:pkg': handler,
+    '/homebrew/:type<formula|cask>/:topic<v|dm|dy>/:pkg': handler
   }
 })
 
-async function handler ({ type = 'formula', pkg }: PathArgs) {
+async function handler ({ type = 'formula', topic, pkg }: PathArgs) {
   const endpoint = `https://formulae.brew.sh/api/${type}/${pkg}.json`
-  const subject = type === 'cask' ? 'homebrew cask' : 'homebrew'
   const {
+    analytics,
     versions,
     version:ver = versions.stable
   } = await got(endpoint).json<any>()
 
-  return {
-    subject,
-    status: version(ver),
-    color: versionColor(ver)
+  switch (topic) {
+    case 'v':
+      return {
+        subject: type === 'cask' ? 'homebrew cask' : 'homebrew',
+        status: version(ver),
+        color: versionColor(ver)
+      }
+    case 'dm':
+      return {
+        subject: 'downloads',
+        status: millify(analytics.install['30d'][pkg]) + '/month',
+        color: 'green'
+      }
+    case 'dy':
+      return {
+        subject: 'downloads',
+        status: millify(analytics.install['365d'][pkg]) + '/year',
+        color: 'green'
+      }
   }
 }
