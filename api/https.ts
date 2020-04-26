5a8e5cdc778568f682519a50b28a59ea74fbd5f4
Steven <steven@ceriously.com>
2020-04-26 09:29:13 -0400

    chore: rename ZEIT to Vercel (#379)

--- api/https.ts
+++ api/https.ts
@@ -4,7 +4,7 @@ import { createBadgenHandler, PathArgs } from '../libs/create-badgen-handler'
 const help = `
 ## Use Badgen with HTTPS Endpoint
 
-1. Create a https endpoint with [RunKit][runkit-href] / [Now][now-href]
+1. Create a https endpoint with [RunKit][runkit-href] / [Vercel][vercel-href]
 or any platform, which returns a JSON in this format:
 
    \`\`\`
@@ -38,7 +38,7 @@ Furthermore, you may append path args to it:
 - https://badgen.net/https/cal-badge-icd0onfvrxx6.runkit.sh/Asia/Shanghai
 
 [runkit-href]: https://runkit.com/home#endpoint
-[now-href]: https://zeit.co/now
+[vercel-href]: https://vercel.com
 
 <style>
   li a { font-family: monospace; font-size: 0.9em }
