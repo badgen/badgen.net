d0d2514d8a61d84108f192c24771298652f4ae14
lucashalbert <lhalbert@lhalbert.xyz>
2020-10-21 18:32:27 -0400

    feat(docker): Add image layers handler

--- api/docker.ts
+++ api/docker.ts
@@ -1,5 +1,6 @@
 import millify from 'millify'
 import got from '../libs/got'
+import { getDockerAuthToken, getDockerFatManifest, getDockerImageManifest } from '../libs/docker'
 import { createBadgenHandler, PathArgs } from '../libs/create-badgen-handler'
 
 export default createBadgenHandler({
@@ -12,10 +13,12 @@ export default createBadgenHandler({
     '/docker/stars/library/mongo?icon=docker&label=stars': 'stars (icon & label)',
     '/docker/size/lukechilds/bitcoind/latest/amd64': 'size (scoped/tag/architecture)',
     '/docker/size/lucashalbert/curl/latest/arm/v6': 'size (scoped/tag/architecture/variant)',
+    '/docker/layers/lucashalbert/curl/latest/arm/v6': 'layers (scoped/tag/architecture/variant)',
   },
   handlers: {
     '/docker/:topic<stars|pulls>/:scope/:name': starPullHandler,
-    '/docker/size/:scope/:name/:tag?/:architecture?/:variant?': sizeHandler
+    '/docker/size/:scope/:name/:tag?/:architecture?/:variant?': sizeHandler,
+    '/docker/layers/:scope/:name/:tag?/:architecture?/:variant?': layersHandler,
   }
 })
 
@@ -99,6 +102,71 @@ async function sizeHandler ({ scope, name, tag, architecture, variant }: PathArg
   return {
     subject: 'docker image size',
     status: `${sizeInMegabytes} MB`,
+    color: 'blue'
+  }
+}
+
+async function layersHandler ({ scope, name, tag, architecture, variant }: PathArgs) {
+  tag = tag ? tag : 'latest'
+  architecture = architecture ? architecture : 'amd64'
+  variant = variant ? variant : ''
+
+  const token = (await getDockerAuthToken(scope, name)).token
+  if (! token) {
+    return {
+      subject: 'docker layers',
+      status: 'unknown image',
+      color: 'grey'
+    }
+  }
+
+  const manifests = (await getDockerFatManifest(scope, name, tag, token)).manifests
+  
+  if (! manifests) {
+    return {
+      subject: 'docker layers',
+      status: 'unknown tag',
+      color: 'grey'
+    }
+  }
+
+  let manifest = manifests.find(manifests => manifests.platform.architecture === architecture)
+
+  if (! manifest) {
+    return {
+      subject: 'docker layers',
+      status: 'unknown architecture',
+      color: 'grey'
+    }
+  }
+
+  if (variant) {
+    manifest = manifests.filter(manifests => manifests.platform.architecture === architecture).find(manifests => manifests.platform.variant === variant)
+
+    if (! manifest) {
+      return {
+        subject: 'docker layers',
+        status: 'unknown variant',
+        color: 'grey'
+      }
+    }
+  }
+
+  const digest = manifest.digest
+
+  const layers = (await getDockerImageManifest(scope, name, digest, token)).layers
+
+  if (! layers) {
+    return {
+      subject: 'docker layers',
+      status: 'error getting layers',
+      color: 'grey'
+    }
+  }
+
+  return {
+    subject: 'docker layers',
+    status: `${layers.length}`,
     color: 'blue'
   }
 }
---
+++ libs/docker.ts
@@ -0,0 +1,35 @@
+import got from './got'
+import { BadgenError } from './create-badgen-handler'
+
+// request image specific DockerHub pull token
+export function getDockerAuthToken<T = any>(scope: string, name: string) {
+  const prefixUrl = process.env.DOCKER_AUTHENTICATION_API || 'https://auth.docker.io/'
+  const service = 'registry.docker.io'
+  const searchParams = {
+    service: service,
+    scope: `repository:${scope}/${name}:pull`
+  }
+  return got.get("token", { prefixUrl, searchParams }).json<T>()
+}
+
+// request fat manifest
+export function getDockerFatManifest<T = any>(scope: string, name: string, tag: string, token: string) {
+  const headers = {
+    authorization: `Bearer ${token}`,
+    accept: `application/vnd.docker.distribution.manifest.list.v2+json`
+  }
+  const prefixUrl = process.env.DOCKER_REGISTRY_API || 'https://registry.hub.docker.com/'
+  const path = `v2/${scope}/${name}/manifests/${tag}`
+  return got.get(path, { prefixUrl, headers }).json<T>()
+}
+
+// request specific image manifest
+export function getDockerImageManifest<T = any>(scope: string, name: string, digest: string, token: string) {
+  const headers = {
+    authorization: `Bearer ${token}`,
+    accept: `application/vnd.docker.distribution.manifest.v2+json`
+  }
+  const prefixUrl = process.env.DOCKER_REGISTRY_API || 'https://registry.hub.docker.com/'
+  const path = `v2/${scope}/${name}/manifests/${digest}`
+  return got.get(path, { prefixUrl, headers }).json<T>()
+}
