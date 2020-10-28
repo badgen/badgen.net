4952a0f22fbba100e9b79326c6b65b4185782177
lhalbert <lucashalbert@users.noreply.github.com>
2020-10-27 22:41:13 -0400

    feat(docker): Add image layers handler (#442)
    * feat(docker): Add image layers handler
    
    * Remove unnecessary BadgenError import
    
    * Add metadataHandler and refactor code
    
    * Separate the layers and metadata handlers
    
    Separate the layers and metadata handlers and remove 'metadata' from the layers path.
    
    * revert subject/label to static descriptive text
    
    * Move manifest error checking logic into libs\docker.ts
    
    Move manifest error checking logic into libs\docker.ts to clean up api\docker.ts
    
    * Replace 'throw new BadenError' with grey error badge
    
    Replace 'throw new BadenError' with grey error badge

--- api/docker.ts
+++ api/docker.ts
@@ -1,8 +1,23 @@
 import millify from 'millify'
 import got from '../libs/got'
+import { getDockerAuthToken, getManifestList, getImageManifest, getImageConfig } from '../libs/docker'
 import { createBadgenHandler, PathArgs } from '../libs/create-badgen-handler'
 
+const help = `## Usage
+
+- \`/docker/stars/:scope/:name\`   _stars_
+- \`/docker/pulls/:scope/:name\`   _pulls_
+- \`/docker/size/:scope/:name/:tag?/:architecture?/:variant?\` _size (scoped/tag/architecture/variant)_
+- \`/docker/layers/:scope/:name/:tag?/:architecture?/:variant?\` _layers (scoped/tag/architecture/variant)_
+- \`/docker/metadata/:type/:scope/:name/:tag?/:architecture?/:variant?\` _metadata (type/scoped/tag/architecture/variant)_
+
+### Querying [Label-Schema](http://label-schema.org/) Metadata
+
+Metadata associated with an image in the [label-schema](http://label-schema.org/) format can be queried using the metadata handler. For example, a label of \`org.label-schema.build-date=2020-10-26T14:25:14Z\` on the image \`lucashalbert/curl\` can be queried using the badgen URL of [/docker/metadata/build-date/lucashalbert/curl/](/docker/metadata/build-date/lucashalbert/curl/).
+`
+
 export default createBadgenHandler({
+  help,
   title: 'Docker',
   examples: {
     '/docker/pulls/library/ubuntu': 'pulls (library)',
@@ -12,10 +27,19 @@ export default createBadgenHandler({
     '/docker/stars/library/mongo?icon=docker&label=stars': 'stars (icon & label)',
     '/docker/size/lukechilds/bitcoind/latest/amd64': 'size (scoped/tag/architecture)',
     '/docker/size/lucashalbert/curl/latest/arm/v6': 'size (scoped/tag/architecture/variant)',
+    '/docker/layers/lucashalbert/curl/latest/arm/v7': 'layers (size)',
+    '/docker/layers/lucashalbert/curl/latest/arm/v7?icon=docker&label=layers': 'layers (icon & label)',
+    '/docker/layers/lucashalbert/curl/latest/arm/v7?label=docker%20layers': 'layers (label)',
+    '/docker/metadata/version/lucashalbert/curl/latest/arm64/v8': 'metadata (version)',
+    '/docker/metadata/architecture/lucashalbert/curl/latest/arm64/v8': 'metadata (architecture)',
+    '/docker/metadata/build-date/lucashalbert/curl/latest/arm64/v8': 'metadata (build-date)',
+    '/docker/metadata/maintainer/lucashalbert/curl/latest/arm64/v8': 'metadata (maintainer)',
   },
   handlers: {
     '/docker/:topic<stars|pulls>/:scope/:name': starPullHandler,
-    '/docker/size/:scope/:name/:tag?/:architecture?/:variant?': sizeHandler
+    '/docker/size/:scope/:name/:tag?/:architecture?/:variant?': sizeHandler,
+    '/docker/layers/:scope/:name/:tag?/:architecture?/:variant?': layersHandler,
+    '/docker/metadata/:type/:scope/:name/:tag?/:architecture?/:variant?': metadataHandler,
   }
 })
 
@@ -97,8 +121,66 @@ async function sizeHandler ({ scope, name, tag, architecture, variant }: PathArg
   const sizeInMegabytes = (imageData.size / 1024 / 1024).toFixed(2)
 
   return {
-    subject: 'docker image size',
+    subject: 'docker size',
     status: `${sizeInMegabytes} MB`,
+    color: 'blue'
+  }
+}
+
+async function layersHandler ({ scope, name, tag, architecture, variant }: PathArgs) {
+  tag = tag ? tag : 'latest'
+  architecture = architecture ? architecture : 'amd64'
+  variant = variant ? variant : ''
+  
+  const token = (await getDockerAuthToken(scope, name)).token
+
+  const manifest_list = await getManifestList(scope, name, tag, architecture, variant, token)
+
+  const image_manifest = await getImageManifest(scope, name, manifest_list.digest, token)
+
+  const image_config = await getImageConfig(scope, name, image_manifest.config.digest, token)
+
+  const layers = image_config.history
+  if (! layers) {
+    return {
+      subject: 'docker layers',
+      status: 'error getting layers',
+      color: 'grey'
+    }
+  }
+
+  return {
+    subject: 'docker layers',
+    status: `${layers.length}`,
+    color: 'blue'
+  }
+}
+
+async function metadataHandler ({ type, scope, name, tag, architecture, variant }: PathArgs) {
+  tag = tag ? tag : 'latest'
+  architecture = architecture ? architecture : 'amd64'
+  variant = variant ? variant : ''
+  
+  const token = (await getDockerAuthToken(scope, name)).token
+
+  const manifest_list = await getManifestList(scope, name, tag, architecture, variant, token)
+
+  const image_manifest = await getImageManifest(scope, name, manifest_list.digest, token)
+
+  const image_config = await getImageConfig(scope, name, image_manifest.config.digest, token)
+
+  const metadata = image_config.container_config.Labels[`org.label-schema.${type}`]
+  if (! metadata) {
+    return {
+      subject: 'docker metadata',
+      status: `error getting ${type}`,
+      color: 'grey'
+    }
+  }
+
+  return {
+    subject: `${type}`,
+    status: `${metadata}`,
     color: 'blue'
   }
 }
