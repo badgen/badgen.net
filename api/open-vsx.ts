d5b735d56b39dd4a27b875dcacfc3ca4a74c9e77
idleberg <git@idleberg.com>
2021-03-11 21:39:41 +0100

    remove topic shortcuts

--- api/open-vsx.ts
+++ api/open-vsx.ts
@@ -6,13 +6,14 @@ import { createBadgenHandler, PathArgs } from '../libs/create-badgen-handler'
 export default createBadgenHandler({
   title: 'Open VSX',
   examples: {
-    '/open-vsx/v/idleberg/electron-builder': 'version',
-    '/open-vsx/r/idleberg/electron-builder': 'reviews',
-    '/open-vsx/l/idleberg/electron-builder': 'license',
-    '/open-vsx/d/idleberg/electron-builder': 'downloads'
+    '/open-vsx/version/idleberg/electron-builder': 'version',
+    '/open-vsx/rating/idleberg/electron-builder': 'rating',
+    '/open-vsx/reviews/idleberg/electron-builder': 'reviews',
+    '/open-vsx/license/idleberg/electron-builder': 'license',
+    '/open-vsx/downloads/idleberg/electron-builder': 'downloads'
   },
   handlers: {
-    '/open-vsx/:topic/:namespace/:pkg': handler
+    '/open-vsx/:topic<downloads|license|rating|reviews|version>/:namespace/:pkg': handler
   }
 })
 
@@ -21,21 +22,18 @@ async function handler ({ topic, pkg, namespace }: PathArgs) {
   const data = await got(endpoint).json<any>()
 
   switch (topic) {
-    case 'v':
     case 'version':
       return {
         subject: `version`,
         status: version(data.version),
         color: versionColor(data.version)
       }
-    case 'd':
     case 'downloads':
       return {
         subject: 'downloads',
         status: millify(data.downloadCount),
         color: 'green'
       }
-    case 'l':
     case 'license':
       return {
         subject: 'license',
@@ -48,7 +46,6 @@ async function handler ({ topic, pkg, namespace }: PathArgs) {
         status: `${data.averageRating}/5`,
         color: 'green'
       }
-    case 'r':
     case 'reviews':
       return {
         subject: 'reviews',
