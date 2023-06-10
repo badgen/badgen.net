915457547cb23e4fc019beb43f16fab43b4de784
Amio <amio.cn@gmail.com>
2023-06-10 09:16:06 +0800

    chore: add github badge examples to badge builder

--- libs/badge-list2.ts
+++ libs/badge-list2.ts
@@ -1,9 +1,11 @@
 import staticBadge from '../pages/api/static'
+import github from '../pages/api/github'
 import winget from '../pages/api/winget'
 import xo from '../pages/api/xo'
 
 export default {
   static: staticBadge.meta,
+  github: github.meta,
   winget: winget.meta,
   xo: xo.meta
 }
