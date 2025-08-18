b49fe3a02da929aa7f8fd6f7decdb0d37d928b70
Amio <amio.cn@gmail.com>
2025-08-18 10:59:32 +0800

    chore: clean semicolons

--- libs/gitlab.ts
+++ libs/gitlab.ts
@@ -2,13 +2,12 @@ import got from './got'
 
 const rand = <T>(arr: T[]): T => arr[Math.floor(Math.random() * arr.length)]
 
-
 // request gitlab api v4 (graphql)
 export function queryGitlab<T = any>(query) {
-  const token = pickGitlabToken(); 
+  const token = pickGitlabToken()
   const headers = {
     authorization: token ? `Bearer ${token}` : undefined,
-  };
+  }
   const json = { query }
   const endpoint =
     process.env.GITLAB_API_GRAPHQL || 'https://gitlab.com/api/graphql'
@@ -16,10 +15,10 @@ export function queryGitlab<T = any>(query) {
 }
 
 export function restGitlab<T = any>(path: string, fullResponse = false) {
-  const token = pickGitlabToken(); 
+  const token = pickGitlabToken()
   const headers = {
     authorization: token ? `Bearer ${token}` : undefined,
-  };
+  }
   const prefixUrl = process.env.GITLAB_API || 'https://gitlab.com/api/v4'
   return fullResponse ? got.get(path, { prefixUrl, headers }) : got.get(path, { prefixUrl, headers }).json<T>()
 }
@@ -27,7 +26,7 @@ export function restGitlab<T = any>(path: string, fullResponse = false) {
 function pickGitlabToken() {
   const { GITLAB_TOKENS } = process.env
   if (!GITLAB_TOKENS) {
-    return null;
+    return null
   }
   const tokens = GITLAB_TOKENS.split(',').map(segment => segment.trim())
   return rand(tokens)
