35d33c4b6e473d437495fab62eab31b4a7a6bdca
Amio Jin <amio.cn@gmail.com>
2026-02-13 18:25:34 +0800

    feat: deprecate codeclimate (#743)

--- pages/api/codeclimate.ts
+++ pages/api/codeclimate.ts
@@ -4,9 +4,17 @@ import { createBadgenHandler, PathArgs } from '../../libs/create-badgen-handler-
 import type { BadgenParams } from '../../libs/types'
 
 
+const help = `
+## Discontinued
+
+CodeClimate has now been deprecated in favour of Qlty ([announcement](https://codeclimate.com/blog/code-climate-quality-is-now-qlty-software)).
+
+Qlty now has an official badge service documented [here](https://docs.qlty.sh/coverage/status-badges).
+`
 
 export default createBadgenHandler({
   title: 'Code Climate',
+  help,
   examples: {
     '/codeclimate/loc/codeclimate/codeclimate': 'lines of code',
     '/codeclimate/issues/codeclimate/codeclimate': 'issues',
@@ -22,110 +30,9 @@ export default createBadgenHandler({
 })
 
 async function handler ({ topic, owner, repo }: PathArgs): Promise<BadgenParams> {
-  const api = `https://api.codeclimate.com/v1/repos?github_slug=${owner}/${repo}`
-  const result = await got(api).json<any>()
-  const meta = result.data[0]
-
-  if (!meta) {
-    return {
-      subject: 'codeclimate',
-      status: 'not found',
-      color: 'grey'
-    }
+  return {
+    subject: 'codeclimate',
+    status: 'discontinued',
+    color: 'grey'
   }
-
-  const testReport = meta.relationships.latest_default_branch_test_report.data
-  const snapReport = meta.relationships.latest_default_branch_snapshot.data
-
-  switch (topic) {
-    case 'loc':
-    case 'issues':
-    case 'tech-debt':
-    case 'maintainability':
-    case 'maintainability-percentage':
-      return getReport(meta.id, snapReport.id, 'snapshots', topic)
-    case 'coverage-letter':
-    case 'coverage':
-      if (!testReport) return {
-        subject: 'codeclimate',
-        status: 'no test',
-        color: 'grey'
-      }
-      return getReport(meta.id, testReport.id, 'test_reports', topic)
-    default:
-      return {
-        subject: 'codeclimate',
-        status: 'unknown topic',
-        color: 'grey'
-      }
-  }
-}
-
-const getReport = async (repoId, reportId, type, topic) => {
-  const api = `https://api.codeclimate.com/v1/repos/${repoId}/${type}/${reportId}`
-  const { data: { meta, attributes } } = await got(api).json<any>()
-
-  switch (topic) {
-    case 'issues':
-      return {
-        subject: 'issues',
-        status: meta.issues_count,
-        color: 'blue'
-      }
-    case 'tech-debt':
-      return {
-        subject: 'technical debt',
-        status: meta.measures.technical_debt_ratio.value.toFixed() + '%',
-        color: debtColors(meta.measures.technical_debt_ratio.value)
-      }
-    case 'loc':
-      return {
-        subject: 'lines of code',
-        status: attributes.lines_of_code,
-        color: 'blue'
-      }
-    case 'maintainability':
-      return {
-        subject: 'maintainability',
-        status: attributes.ratings[0].letter,
-        color: gradeColors[attributes.ratings[0].letter] || 'red'
-      }
-    case 'maintainability-percentage':
-      return {
-        subject: 'maintainability',
-        status: attributes.ratings[0].measure.value.toFixed() + '%',
-        color: gradeColors[attributes.ratings[0].letter] || 'red'
-      }
-    case 'coverage-letter':
-      return {
-        subject: 'coverage',
-        status: attributes.rating.letter,
-        color: scale('coverage')(attributes.rating.measure.value)
-      }
-    case 'coverage':
-      return {
-        subject: 'coverage',
-        status: cov(attributes.rating.measure.value),
-        color: scale('coverage')(attributes.rating.measure.value)
-      }
-    default:
-      return {
-        subject: 'codeclimate',
-        status: 'unknown topic',
-        color: 'gray'
-      }
-  }
-}
-
-const debtColors = scale(
-  [5, 10, 20, 50],
-  ['green', '9C1', 'EA2', 'orange', 'red']
-)
-
-const gradeColors = {
-  'A': 'green',
-  'B': '9C0',
-  'C': 'AA2',
-  'D': 'DC2',
-  'E': 'orange'
 }
