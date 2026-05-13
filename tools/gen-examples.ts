58a36677994c7011665d80aefbdc1bece91ae675
Amio <amio.cn@gmail.com>
2026-05-13 18:46:49 +0800

    refactor: remove deprecated badge-list legacy metadata path

--- tools/gen-examples.ts
+++ tools/gen-examples.ts
@@ -1,18 +1,10 @@
 import path from 'path'
 import fse from 'fs-extra'
-import { loadBadgeMeta } from '../libs/badge-list'
 import BadgeList2 from '../libs/badge-list2'
 
 const rel = (...args) => path.resolve(__dirname, ...args)
 
 ;(async function main () {
-  // old list
-  const badgeMeta = await loadBadgeMeta()
-  await fse.outputJson(rel('../public/.meta/badges.json'), badgeMeta, {
-    spaces: 2
-  })
-
-  // new list
   await fse.outputJson(rel('../public/.meta/badge-list.json'), BadgeList2, {
     spaces: 2
   })
