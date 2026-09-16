d127a2ed66936cc33b323be8e273e4d053c4275e
Amio <amio.cn@gmail.com>
2026-09-16 15:58:00 +0800

    chore(deps): update dependencies after upstream sync

--- next.config.js
+++ next.config.js
@@ -1,23 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const { withSentryConfig } = require('@sentry/nextjs')
+const { withSentryConfig } = require('@sentry/nextjs/config')
 
 const nextConfig = {
   reactStrictMode: true,
-  optimizeFonts: false,
-
-  experimental: {
-  },
-
-  sentry: {
-    // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
-    // for client-side builds. (This will be the default starting in
-    // `@sentry/nextjs` version 8.0.0.) See
-    // https://webpack.js.org/configuration/devtool/ and
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
-    // for more information.
-    hideSourceMaps: true,
-  },
 
   async rewrites() {
     const badgeApis = [
